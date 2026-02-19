05802b2637c6b4142749b54203b8c2b543ed4b28
Litezy <liteb237@gmail.com>
2026-02-19 09:10:58 +0100

    fix: made some fixes

--- School Mgt/ExcelSchool.sol
+++ School Mgt/ExcelSchool.sol
@@ -7,6 +7,7 @@ contract ExcelSchool {
     address admin;
 
     mapping(uint8 => uint256) public levelPrice;
+    mapping(address => Roles) userRoles;
 
     constructor() {
         admin = msg.sender;
@@ -14,6 +15,7 @@ contract ExcelSchool {
         levelPrice[2] = 0.025 ether;
         levelPrice[3] = 0.05 ether;
         levelPrice[4] = 0.09 ether;
+        userRoles[admin] = Roles.admin;
     }
 
     modifier onlyAdmin() {
@@ -50,8 +52,8 @@ contract ExcelSchool {
         address wallet;
         uint8 level;
         uint8 age;
-        uint amoutPaid;
-        Status status;
+        uint amountPaid;
+        Status paymentStatus;
         Roles role;
         bool claimed;
         uint claimedAt;
@@ -68,51 +70,54 @@ contract ExcelSchool {
         _;
     }
 
-    
-
-
     //mappings
     mapping(address => Student) public studentDetails;
     mapping(address => uint256) lastPaid;
+    uint256 public schoolTreasury;
     uint256 constant PAY_INTERVAL = 30 days;
     mapping(address => Staff) public staffDetails;
     mapping(address => uint256) public schoolAccount;
 
-
     //check if paid recently modifier
-    modifier checkIfStaffIsPaidRecently( address _staff_wallet) {
-        require(staffDetails[_staff_wallet].role == Roles.staff,"Only staff are paid");
-         uint _now = block.timestamp;
-         uint last = lastPaid[_staff_wallet];
-         require(_now < last + PAY_INTERVAL,"Paid recently");
+    modifier checkIfStaffIsPaidRecently(address _staff_wallet) {
+        require(
+            staffDetails[_staff_wallet].role == Roles.staff,
+            "Only staff are paid"
+        );
+        uint _now = block.timestamp;
+        uint last = lastPaid[_staff_wallet];
+        require(_now < last + PAY_INTERVAL, "Paid recently");
         _;
     }
 
-    Staff[] public staff;
-    Student[] public students;
-    mapping(address => Roles) userRoles;
+    Staff[] public staffList;
+    Student[] public studentList;
 
-    function addStudent(string memory _name,uint8 _level,uint8 _age) public onlyAdmin {
+    function addStudent(
+        string memory _name,
+        uint8 _level,
+        uint8 _age
+    ) public onlyAdmin {
         require(_level > 0 && _level <= 4, "Level Exceeded");
-        uint index = students.length;
+        uint index = studentList.length;
         Student memory newStudent = Student({
             id: index,
             name: _name,
             wallet: address(0),
             level: _level,
             age: _age,
-            amoutPaid: 0,
-            status: Status.unpaid,
+            amountPaid: 0,
+            paymentStatus: Status.unpaid,
             role: Roles.student,
             claimed: false,
-            claimedAt:0
+            claimedAt: 0
         });
-        students.push(newStudent);
+        studentList.push(newStudent);
         emit Events.StudentEvent("student added sucessfully", newStudent.id);
     }
 
-    function addStaff( string memory _name, uint _salary) public onlyAdmin {
-        uint _id = staff.length;
+    function addStaff(string memory _name, uint _salary) public onlyAdmin {
+        uint _id = staffList.length;
         Staff memory newStaff = Staff({
             id: _id,
             name: _name,
@@ -122,33 +127,34 @@ contract ExcelSchool {
             paidAt: 0,
             role: Roles.staff,
             claimed: false,
-            claimedAt:0
+            claimedAt: 0
         });
-        staff.push(newStaff);
-        emit Events.StaffEvent(newStaff.id,newStaff.wallet);
+        staffList.push(newStaff);
+        emit Events.StaffEvent(newStaff.id, newStaff.wallet);
     }
 
-    function claimStaffId(uint _Id) external  onlyStaff(Roles.staff) {
-        require(_Id < staff.length, "Staff not found");
+    function claimStaffId(uint _Id) external {
+        require(_Id < staffList.length, "Staff not found");
 
-        Staff storage unClaimedStaff = staff[_Id];
+        Staff storage unClaimedStaff = staffList[_Id];
 
         require(!unClaimedStaff.claimed, "Already claimed");
 
         unClaimedStaff.wallet = msg.sender;
         unClaimedStaff.claimed = true;
         unClaimedStaff.claimedAt = block.timestamp;
-
+        staffDetails[unClaimedStaff.wallet];
+        userRoles[msg.sender] = Roles.staff;
 
         emit Events.StaffEvent(_Id, unClaimedStaff.wallet);
     }
 
+    function claimStudentId(
+        uint _Id
+    ) external payable onlyStudent(Roles.student) {
+        require(_Id < studentList.length, "Student not found");
 
-
-    function claimStudentId( uint _Id) external payable onlyStudent(Roles.student) {
-        require(_Id < students.length, "Student not found");
-
-        Student storage student = students[_Id];
+        Student storage student = studentList[_Id];
 
         require(!student.claimed, "Already claimed");
 
@@ -156,22 +162,68 @@ contract ExcelSchool {
         require(fee > 0, "Invalid level");
         require(msg.value == fee, "Wrong amount");
 
-       schoolAccount[msg.sender] = schoolAccount[msg.sender] + msg.value;
+        schoolTreasury = schoolTreasury + msg.value;
         student.wallet = msg.sender;
 
-        student.amoutPaid = msg.value;
+        student.amountPaid = msg.value;
 
         student.claimed = true;
         student.claimedAt = block.timestamp;
+        student.paymentStatus = Status.paid;
+        studentDetails[student.wallet];
+        userRoles[msg.sender] = Roles.student;
 
         emit Events.StudentClaimEvent(_Id, msg.value, student.wallet);
     }
 
-    function payStaff (uint _staffId,address _wallet) external payable onlyAdmin checkIfStaffIsPaidRecently(_wallet) {
-      
+    function payStaff(
+        address _wallet
+    ) external onlyAdmin checkIfStaffIsPaidRecently(_wallet) {
+        Staff storage _staff = staffDetails[_wallet];
+        require(
+            _staff.wallet != address(0),
+            "Not staff/Unclaimed staff profile"
+        );
+        require(_staff.salary > 0, "Salary can't be 0 ether");
+        require(
+            schoolTreasury >= _staff.salary,
+            "Insufficient funds in treasury"
+        );
+        schoolTreasury -= _staff.salary;
+        lastPaid[_wallet] = block.timestamp;
+        _staff.paid = true;
+        _staff.paidAt = block.timestamp;
+        (bool success, ) = payable(_wallet).call{value: _staff.salary}("");
+        require(success);
+    }
+
+    function getStudent(address _wallet) public view returns (Student memory) {
+        return studentDetails[_wallet];
+    }
+
+    function getStaff(address _wallet) public view returns (Staff memory) {
+        return staffDetails[_wallet];
+    }
+
+    function getAllStaff() public view returns (Staff[] memory) {
+        Staff[] memory allStaff = new Staff[](staffList.length);
+
+        for (uint i = 0; i < staffList.length; i++) {
+            allStaff[i] = staffDetails[staffList[i].wallet];
+        }
+
+        return allStaff;
+    }
+
+    function getAllstudentList() public view returns (Student[] memory) {
+         Student[] memory allStudents = new Student[](studentList.length);
+
+        for (uint i = 0; i < studentList.length; i++) {
+            allStudents[i] = studentDetails[studentList[i].wallet];
+        }
+
+        return allStudents;
     }
 
     receive() external payable {}
-
-    fallback() external payable {}
 }
